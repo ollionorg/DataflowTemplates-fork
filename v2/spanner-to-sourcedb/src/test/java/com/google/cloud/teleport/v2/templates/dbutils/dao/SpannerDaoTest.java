4ae0b697193a8c65011a7d1405fdbc162159b399
Akash Thawait <aakash@ollion.com>
2024-11-28 13:59:54 +0530

    removed white spaces

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/dbutils/dao/SpannerDaoTest.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/dbutils/dao/SpannerDaoTest.java
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.google.cloud.teleport.v2.templates.dbutils.dao;
 
 import static com.google.common.truth.Truth.assertThat;

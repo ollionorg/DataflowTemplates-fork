ac6bde7e5f14c92ccd8c213893294f2c6dc3c42e
Akash Thawait <aakash@ollion.com>
2024-12-02 16:07:58 +0530

    Added Cassandra Dao Class

---
+++ v2/spanner-to-sourcedb/src/main/java/com/google/cloud/teleport/v2/templates/dbutils/dao/source/CassandraDao.java
@@ -0,0 +1,46 @@
+package com.google.cloud.teleport.v2.templates.dbutils.dao.source;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.google.cloud.teleport.v2.templates.dbutils.connection.IConnectionHelper;
+import com.google.cloud.teleport.v2.templates.exceptions.ConnectionException;
+
+public class CassandraDao implements IDao<String> {
+  private final String cassandraUrl;
+  private final IConnectionHelper connectionHelper;
+
+  public CassandraDao(String cassandraUrl, String cassandraUser, IConnectionHelper connectionHelper) {
+    this.cassandraUrl = cassandraUrl;
+    this.connectionHelper = connectionHelper;
+  }
+
+  @Override
+  public void write(String cqlStatement) throws Exception {
+    CqlSession session = null;
+
+    try {
+      session = (CqlSession) connectionHelper.getConnection(this.cassandraUrl);
+      if (session == null) {
+        throw new ConnectionException("Connection is null");
+      }
+      SimpleStatement statement = SimpleStatement.newInstance(cqlStatement);
+      session.execute(statement);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public String read(String cqlStatement) throws Exception {
+
+    try (CqlSession session = (CqlSession) connectionHelper.getConnection(this.cassandraUrl)) {
+      if (session == null) {
+        throw new ConnectionException("Connection is null");
+      }
+      SimpleStatement statement = SimpleStatement.newInstance(cqlStatement);
+      return session.execute(statement).toString();
+    }
+  }
+}

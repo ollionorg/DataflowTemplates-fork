5bd8ec5f9fcba46a805b5b6ef8d1b4dc5d2e1bfe
pawankashyapollion <v-pawan.kumar@ollion.com>
2025-01-20 10:56:57 +0530

    Added IT for Spanner to Cassandra SOurce DB

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToCassandraSourceLT.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToCassandraSourceLT.java
@@ -1,12 +1,33 @@
+/*
+ * Copyright (C) 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.cloud.teleport.v2.templates;
 
+import static org.apache.beam.it.gcp.artifacts.utils.ArtifactUtils.getFullGcsPath;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatPipeline;
+import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
+
 import com.google.cloud.teleport.metadata.TemplateLoadTest;
+import java.io.IOException;
+import java.text.ParseException;
+import java.time.Duration;
 import org.apache.beam.it.cassandra.CassandraResourceManager;
 import org.apache.beam.it.common.PipelineLauncher;
 import org.apache.beam.it.common.PipelineOperator;
 import org.apache.beam.it.common.TestProperties;
 import org.apache.beam.it.gcp.datagenerator.DataGenerator;
-import org.apache.beam.it.jdbc.conditions.JDBCRowsCheck;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,18 +38,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.shaded.com.google.common.io.Resources;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.time.Duration;
-
-import static org.apache.beam.it.gcp.artifacts.utils.ArtifactUtils.getFullGcsPath;
-import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatPipeline;
-import static org.apache.beam.it.truthmatchers.PipelineAsserts.assertThatResult;
-
 @Category(TemplateLoadTest.class)
 @TemplateLoadTest(SpannerToSourceDb.class)
 @RunWith(JUnit4.class)
-
 public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
   private static final Logger LOG = LoggerFactory.getLogger(SpannerToCassandraSourceLT.class);
 
@@ -36,7 +48,8 @@ public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
   private final String artifactBucket = TestProperties.artifactBucket();
   private final String spannerDdlResource = "SpannerToCassandraSourceLT/spanner-schema.sql";
   private final String sessionFileResource = "SpannerToCassandraSourceLT/session.json";
-  private final String dataGeneratorSchemaResource = "SpannerToCassandraSourceLT/datagenerator-schema.json";
+  private final String dataGeneratorSchemaResource =
+      "SpannerToCassandraSourceLT/datagenerator-schema.json";
   private final String table = "Person";
   private final int maxWorkers = 50;
   private final int numWorkers = 20;
@@ -54,21 +67,21 @@ public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
             gcsResourceManager
                 .uploadArtifact(
                     "input/schema.json",
-                    Resources.getResource(dataGeneratorSchemaResource).getPath()
-                ).name()
-        );
+                    Resources.getResource(dataGeneratorSchemaResource).getPath())
+                .name());
 
     createCassandraSchema(cassandraResourceManager);
     jobInfo = launchDataflowJob(artifactBucket, numWorkers, maxWorkers);
   }
 
   @After
-  public void teardown(){
+  public void teardown() {
     cleanupResourceManagers();
   }
 
   @Test
-  public void reverseReplication1KTpsLoadTest() throws IOException, ParseException, InterruptedException {
+  public void reverseReplication1KTpsLoadTest()
+      throws IOException, ParseException, InterruptedException {
     // Start data generator
     DataGenerator dataGenerator =
         DataGenerator.builderWithSchemaLocation(testName, generatorSchemaPath)
@@ -87,12 +100,11 @@ public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
     dataGenerator.execute(Duration.ofMinutes(90));
     assertThatPipeline(jobInfo).isRunning();
 
-//    Todo Check for cassandra
-//    JDBCRowsCheck check =
-//        JDBCRowsCheck.builder(jdbcResourceManagers.get(0), table)
-//            .setMinRows(300000)
-//            .setMaxRows(300000)
-//            .build();
+    CassandraRowsCheck check =
+        CassandraRowsCheck.builder(cassandraResourceManager, table)
+            .setMinRows(300000)
+            .setMaxRows(300000)
+            .build();
 
     PipelineOperator.Result result =
         pipelineOperator.waitForCondition(
@@ -109,20 +121,19 @@ public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
     exportMetrics(jobInfo, numShards);
   }
 
-  private void createCassandraSchema(CassandraResourceManager cassandraResourceManager){
+  private void createCassandraSchema(CassandraResourceManager cassandraResourceManager) {
     String keyspace = cassandraResourceManager.getKeyspaceName();
-    String createTableStatement = String.format(
-        "CREATE TABLE IF NOT EXISTS %s.%s ("
-            + "ID uuid PRIMARY KEY, "
-            + "first_name1 text, "
-            + "last_name1 text, "
-            + "first_name2 text, "
-            + "last_name2 text, "
-            + "first_name3 text, "
-            + "last_name3 text);",
-        keyspace, table
-    );
+    String createTableStatement =
+        String.format(
+            "CREATE TABLE IF NOT EXISTS %s.%s ("
+                + "ID uuid PRIMARY KEY, "
+                + "first_name1 text, "
+                + "last_name1 text, "
+                + "first_name2 text, "
+                + "last_name2 text, "
+                + "first_name3 text, "
+                + "last_name3 text);",
+            keyspace, table);
     cassandraResourceManager.executeStatement(createTableStatement);
   }
 }
-

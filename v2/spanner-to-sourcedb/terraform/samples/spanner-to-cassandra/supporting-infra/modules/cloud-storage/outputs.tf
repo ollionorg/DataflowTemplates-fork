58d1718a216c887cf1f308ef205bdb36ff4cfd10
taherkl <taher.lakdawala@ollion.com>
2025-01-29 10:20:36 +0530

    removing supporting infra folder

--- v2/spanner-to-sourcedb/terraform/samples/spanner-to-cassandra/supporting-infra/modules/cloud-storage/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "bucket_url" {
-  description = "Storage Bucket url"
-  value       = google_storage_bucket.bucket.url
-}
-output "bucket_name" {
-  description = "Storage Bucket Name"
-  value       = google_storage_bucket.bucket.name
-}

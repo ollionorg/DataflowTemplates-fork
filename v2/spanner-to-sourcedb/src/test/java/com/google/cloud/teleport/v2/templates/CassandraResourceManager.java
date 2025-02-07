17f837f08ec763dfda9ab188ebc55f9e44fe9a19
taherkl <taher.lakdawala@ollion.com>
2025-02-07 11:13:57 +0530

    run test for 300K for 1 hour

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/CassandraResourceManager.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/CassandraResourceManager.java
@@ -151,7 +151,9 @@ public class CassandraResourceManager extends TestContainerResourceManager<Gener
           .get(
               () ->
                   cassandraClient.execute(
-                      SimpleStatement.newInstance(statement).setKeyspace(this.keyspaceName).setTimeout(Duration.ofSeconds(30))));
+                      SimpleStatement.newInstance(statement)
+                          .setKeyspace(this.keyspaceName)
+                          .setTimeout(Duration.ofSeconds(30))));
     } catch (Exception e) {
       throw new IllegalArgumentException("Error reading collection.", e);
     }

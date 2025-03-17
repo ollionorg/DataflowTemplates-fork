465ba7d6638f2f4e672a71173eeba300a7fb5848
Akash Thawait <aakash@ollion.com>
2025-03-17 15:14:00 +0530

    fixed spanner schema

--- v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraMaxNonKeyColumnLT/spanner-schema.sql
+++ v2/spanner-to-sourcedb/src/test/resources/SpannerToCassandraMaxNonKeyColumnLT/spanner-schema.sql
@@ -40,3 +40,9 @@ CREATE TABLE maxNonKeyCol (
   col38 STRING(MAX),
   col39 STRING(MAX)
 ) PRIMARY KEY (id);
+
+CREATE CHANGE STREAM allstream
+  FOR ALL OPTIONS (
+  value_capture_type = 'NEW_ROW',
+  retention_period = '7d'
+);

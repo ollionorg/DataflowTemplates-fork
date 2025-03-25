5a1fbe44bec70d2e9dc84968dd0b7e4dfc5ccee0
pawankashyapollion <v-pawan.kumar@ollion.com>
2025-03-26 00:58:44 +0530

    enabled host and proxy prot

--- cicd/cmd/run-it-tests/main.go
+++ cicd/cmd/run-it-tests/main.go
@@ -72,10 +72,10 @@ func main() {
 		flags.RetryFailures(),
 		// flags.StaticOracleHost(),
 		// flags.StaticOracleSysPassword(),
-		// flags.CloudProxyHost(),
-		// flags.CloudProxyMySqlPort(),
+		flags.CloudProxyHost(),
+		flags.CloudProxyMySqlPort(),
 		// flags.CloudProxyPostgresPort(),
-		// flags.CloudProxyPassword(),
+		flags.CloudProxyPassword(),
 		flags.UnifiedWorkerHarnessContainerImage())
 		// flags.CloudProxyPassword())
 	if err != nil {

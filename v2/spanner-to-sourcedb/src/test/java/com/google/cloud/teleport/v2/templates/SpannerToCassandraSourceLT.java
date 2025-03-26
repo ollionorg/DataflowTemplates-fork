580c2807c83f2b6e686e18884e41e0a21e38fba2
taherkl <taher.lakdawala@ollion.com>
2025-03-26 23:11:20 +0530

    custom cassandra

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToCassandraSourceLT.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToCassandraSourceLT.java
@@ -52,8 +52,8 @@ public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
   private final String dataGeneratorSchemaResource =
       "SpannerToCassandraSourceLT/datagenerator-schema.json";
   private final String table = "person";
-  private final int maxWorkers = 1000;
-  private final int numWorkers = 500;
+  private final int maxWorkers = 1500;
+  private final int numWorkers = 1000;
   private PipelineLauncher.LaunchInfo jobInfo;
   private final int numShards = 1;
 

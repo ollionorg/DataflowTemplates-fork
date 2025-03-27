4e907d1f81d182bb46195963cacfdd5f5395786e
taherkl <taher.lakdawala@ollion.com>
2025-03-27 13:54:03 +0530

    increase spanner

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToCassandraSourceLT.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToCassandraSourceLT.java
@@ -52,7 +52,7 @@ public class SpannerToCassandraSourceLT extends SpannerToCassandraLTBase {
   private final String dataGeneratorSchemaResource =
       "SpannerToCassandraSourceLT/datagenerator-schema.json";
   private final String table = "person";
-  private final int maxWorkers = 300;
+  private final int maxWorkers = 1000;
   private final int numWorkers = 300;
   private PipelineLauncher.LaunchInfo jobInfo;
   private final int numShards = 1;

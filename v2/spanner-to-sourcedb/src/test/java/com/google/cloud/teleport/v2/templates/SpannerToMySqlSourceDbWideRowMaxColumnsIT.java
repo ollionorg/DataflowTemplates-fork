6798512a591e5cf0da0868ca6d3a4f5a8040b11a
pawankashyapollion <v-pawan.kumar@ollion.com>
2025-04-10 15:51:47 +0530

    Re-running the IT

--- v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToMySqlSourceDbWideRowMaxColumnsIT.java
+++ v2/spanner-to-sourcedb/src/test/java/com/google/cloud/teleport/v2/templates/SpannerToMySqlSourceDbWideRowMaxColumnsIT.java
@@ -53,7 +53,6 @@ import org.slf4j.LoggerFactory;
 @Category({TemplateIntegrationTest.class, SkipDirectRunnerTest.class})
 @TemplateIntegrationTest(SpannerToSourceDb.class)
 @RunWith(JUnit4.class)
-@Ignore("This test is disabled currently")
 public class SpannerToMySqlSourceDbWideRowMaxColumnsIT extends SpannerToSourceDbITBase {
 
   private static final Logger LOG =
